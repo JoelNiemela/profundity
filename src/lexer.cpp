eea8daffeb599008bcd007041c68bcdd84b7a121
JoelNiemela <joel@niemela.se>
2022-08-23 14:14:37 -0500

    Add tokens and regex patterns to lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -30,6 +30,10 @@ Token Lexer::next_token() {
 	std::string str = "";
 	str += peak_char();
 
+	if (str[0] == '\0') {
+		return Token{Token::Type::ENDINPUT};
+	}
+
 	while (match_token(str) == Token::Type::ERROR) {
 		pop_char();
 		str += peak_char();
@@ -46,7 +50,46 @@ Token Lexer::next_token() {
 }
 
 static const std::vector<std::pair<Token::Type, std::regex> > token_patterns = {
-	{}
+	// keywords
+	{Token::Type::LET,       std::regex("let")},
+	// literals
+	{Token::Type::ID,        std::regex("[_a-zA-Z]+[_a-zA-Z0-9]*")},
+	{Token::Type::NUM,       std::regex("[0-9]*\\.?[0-9]+")},
+	{Token::Type::STRING,    std::regex("/\"(?:[^\"\\\\]|\\\\.)*\"/")},
+	// brackets
+	{Token::Type::LPAR,      std::regex("\\(")},
+	{Token::Type::RPAR,      std::regex("\\)")},
+	{Token::Type::LBRACKET,  std::regex("\\[")},
+	{Token::Type::RBRACKET,  std::regex("\\]")},
+	{Token::Type::LBRACE,    std::regex("\\{")},
+	{Token::Type::RBRACE,    std::regex("\\}")},
+	// arithmetic operators
+	{Token::Type::MUL,       std::regex("\\*")},
+	{Token::Type::DIV,       std::regex("/")},
+	{Token::Type::ADD,       std::regex("\\+")},
+	{Token::Type::SUB,       std::regex("\\-")},
+	// logic operators
+	{Token::Type::EQ,        std::regex("=")},
+	{Token::Type::NE,        std::regex("<>")},
+	{Token::Type::LT,        std::regex("<")},
+	{Token::Type::GT,        std::regex(">")},
+	{Token::Type::LE,        std::regex("<=")},
+	{Token::Type::GE,        std::regex(">=")},
+	// type operators
+	{Token::Type::ARROW,     std::regex("\\->")},
+	{Token::Type::OPT,       std::regex("\\?")},
+	{Token::Type::PTR,       std::regex("\\^")},
+	{Token::Type::REF,       std::regex("&")},
+	// symbols
+	{Token::Type::COLON,     std::regex(":")},
+	{Token::Type::SEMICOLON, std::regex(";")},
+	{Token::Type::COMMA,     std::regex(",")},
+	{Token::Type::DOT,       std::regex("\\.")},
+	{Token::Type::PIPE,      std::regex("|")},
+	{Token::Type::ASSIGN,    std::regex(":=")},
+	// whitespace
+	{Token::Type::NL,        std::regex("\\n+")},
+	{Token::Type::WS,        std::regex("[\\s]*")},
 };
 
 Token::Type Lexer::match_token(std::string token) {
--- src/token.h
+++ src/token.h
@@ -5,7 +5,43 @@
 
 struct Token {
 	enum Type {
-		ERROR
+		// keywords
+		LET,
+		// literals
+		ID,
+		NUM,
+		STRING,
+		// brackets
+		LPAR, RPAR,
+		LBRACKET, RBRACKET,
+		LBRACE, RBRACE,
+		// arithmetic operators
+		MUL,
+		DIV,
+		ADD,
+		SUB,
+		// logic operators
+		EQ, NE,
+		LT, GT,
+		LE, GE,
+		// type operators
+		ARROW,
+		OPT,
+		PTR,
+		REF,
+		// symbols
+		COLON,
+		SEMICOLON,
+		DOT,
+		COMMA,
+		PIPE,
+		ASSIGN,
+		// whitespace
+		NL,
+		WS,
+		// other
+		ENDINPUT,
+		ERROR,
 	} type;
 
 	std::string value;
